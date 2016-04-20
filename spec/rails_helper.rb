365b3b4dff69f53a61b31e86582f6994a8892fe0
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-04-20 18:28:40 -0400

    set up the testing suite.

---
+++ spec/rails_helper.rb
@@ -0,0 +1,39 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+
+require File.expand_path('../../config/environment', __FILE__)
+
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+require 'spec_helper'
+require 'rspec/rails'
+
+require 'shoulda/matchers'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :transaction
+  end
+
+  config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+end
